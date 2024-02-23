4319e189cc788d8cb98927110d689aa130977191
dondoko777 <dondokostar@gmail.com>
2024-02-23 22:26:20 +0900

    Update main.rs

--- ch09/sample-09-02/src/main.rs
+++ ch09/sample-09-02/src/main.rs
@@ -15,10 +15,6 @@ fn main() {
     println!("tri area is {}", tri.calc_area());
     println!("cir area is {}", cir.calc_area());
 
-    println!("rect {} {}", rect.expr_str(), rect.calc_area());
-    println!("tri {} {}", tri.expr_str(), tri.calc_area());
-    println!("cir {} {}", cir.expr_str(), cir.calc_area());
-
 }
 
 // 四角形
@@ -56,42 +52,5 @@ impl CalcArea for Circle {
     fn calc_area(&self) -> f32 {
         self.radius * self.radius * 3.14
     }
-}
-
-// デフォルトメソッドの実装
-trait ExprString {
-    fn expr_str(&self) -> String {
-        "幅 × 高さ = ".to_string()
-    }
-}
-impl ExprString for Rectange {}
-impl ExprString for Triangle {
-    fn expr_str(&self) -> String {
-        "底辺 × 高さ ÷ 2 = ".to_string()
-    }
-}
-impl ExprString for Circle {
-    fn expr_str(&self) -> String {
-        "π × 半径 × 半径 = ".to_string()
-    }
-}
-
-
-trait ToNumber {
-    fn to_i(&self) -> i32 ;
-}
-impl ToNumber for String {
-    fn to_i(&self) -> i32 {
-        match self.parse::<i32>() {
-            Ok(n) => n,
-            Err(_) => 0,
-        }
-    }
-}
-
-fn _main() {
-    let s = String::from("100");
-    let n = s.to_i();
-    println!("n is {}", n );
 }
 
